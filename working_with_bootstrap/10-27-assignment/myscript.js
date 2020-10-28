b414dd76bdacd0361dfd08b4e4d157bc3243855d
dpham-aiu <dannypham1234@gmail.com>
2020-10-28 10:16:23 -0400

    bootstrap and more

---
+++ working_with_bootstrap/10-27-assignment/myscript.js
@@ -0,0 +1,8 @@
+function showForm() {
+  document.getElementById("formRow").style.display = "";
+}
+
+function addNew() {
+  var name = document.container.table.formRow.name.value;
+  alert(name);
+}
